4c4ff83817569af8e1ce0defe56542f646240c9c
PEDRO ALEXANDER ELVIRA GIRON <pelvirag@miumg.edu.gt>
2022-03-29 16:59:02 -0600

    Agregar archivos de proyecto.

---
+++ Tarea_Semana_8/Clases/Carro.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_Semana_8.Clases
+{
+    internal class Carro
+    {
+        public string Marca { get; }
+        public int Modelo { get; }
+        public String Color { get; set; }
+
+        public string owner { get; set; }
+
+        private int Encendido = 0;
+        private int velocidad_actual = 0;
+
+        public int MAXVELOCIDAD { get; }
+
+
+
+
+        public Carro(string _marca, int maxvel)
+        {
+            Marca = _marca;
+            Encendido = 0;
+            MAXVELOCIDAD = maxvel;
+            this.velocidad_actual = 0;
+        }
+
+        //construir 2do constructor
+        public Carro(String _marca, int maxvel, string _owner)
+        {
+            Marca = _marca;
+            MAXVELOCIDAD = maxvel;
+            owner = _owner;
+            this.velocidad_actual = 0;
+        }
+
+        public int GetVelocidadActual()
+        {
+            return velocidad_actual;
+        }
+
+
+        public string EncenderMotor()
+        {
+            if (Encendido == 0)
+            {
+
+                Encendido = 1;
+                this.velocidad_actual = 0;
+                return "run run run run, listo para correr";
+            }
+            else
+            {
+                return "Orales!!! ya esta encendido tu carro";
+            }
+        }
+
+
+        public string acelerar()
+        {
+            string mensaje = "";
+            if (Encendido == 0)
+            {
+                mensaje = $"no puedo acelerar con el carro apagado";
+                return mensaje;
+            }
+
+            velocidad_actual += 10;
+            if (velocidad_actual <= MAXVELOCIDAD)
+            {
+                mensaje = $"Vas a {velocidad_actual} KPH  ";
+            }
+            else
+            {
+                velocidad_actual = MAXVELOCIDAD;
+                mensaje = $"{velocidad_actual}KPH   Aguas rapido y furioso, vas muy rapido y ya llegaste al tope  ";
+            }
+
+
+            return mensaje;
+
+        }
+
+
+
+
+        public string acelerar(int CuantosKPH)
+        {
+            string mensaje = "";
+            if (Encendido == 0)
+            {
+                mensaje = $"no puedo acelerar con el carro apagado";
+                return mensaje;
+            }
+
+            velocidad_actual += CuantosKPH;
+            if (velocidad_actual <= MAXVELOCIDAD)
+            {
+                mensaje = $"{velocidad_actual} KPH ";
+            }
+            else
+            {
+                velocidad_actual = MAXVELOCIDAD;
+                mensaje = $"{velocidad_actual}KPH   Aguas rapido y furioso, vas muy rapido y ya llegaste al tope";
+            }
+
+
+            return mensaje;
+
+        }
+
+
+
+
+    }
+}
+    
---
+++ Tarea_Semana_8/Program.cs
@@ -0,0 +1,100 @@
+﻿using Tarea_Semana_8.Clases;
+
+Carro CarroA;
+Carro CarroB;
+Carro CarroC;
+
+string marca;
+int contadorA = 0;
+int contadorB = 0;
+int contadorC = 0;
+int velocidad;
+string propietario;
+Random velram = new Random();
+velocidad = velram.Next(5, 200);
+for (int i = 0; i < 3; i++)
+{
+    //A
+    Console.WriteLine("Ingrese los datos del equipo A");
+    Console.WriteLine("Ingrese marca");
+    marca = Console.ReadLine();
+    Console.WriteLine("Propietario:");
+    propietario = Console.ReadLine();
+    CarroA = new Carro(marca, velocidad, propietario);
+
+    //B
+    Console.WriteLine("Ahora datos del equipo B");
+    Console.WriteLine("Ingrese marca");
+    marca = Console.ReadLine();
+    Console.WriteLine("Propietario:");
+    propietario = Console.ReadLine();
+    CarroB = new Carro(marca, velocidad, propietario);
+
+    //C
+    Console.WriteLine("Ahora datos del equipo C");
+    Console.WriteLine("Ingrese marca");
+    marca = Console.ReadLine();
+    Console.WriteLine("Propietario:");
+    propietario = Console.ReadLine();
+    CarroC = new Carro(marca, velocidad, propietario);
+
+    string msgCarroA, msgCarroB, msgCarroC;
+    Random velRandom = new Random();
+
+
+
+
+    msgCarroA = CarroA.EncenderMotor();
+    msgCarroB = CarroB.EncenderMotor();
+    msgCarroC = CarroC.EncenderMotor();
+    Console.WriteLine($"{CarroA.Marca} {msgCarroA}");
+    Console.WriteLine($"{CarroB.Marca} {msgCarroB}");
+    Console.WriteLine($"{CarroC.Marca}{msgCarroC}");
+
+
+    for (int ii = 0; ii < 10; ii++)
+    {
+        int acelera;
+        acelera = velRandom.Next(1, 21);
+        msgCarroA = CarroA.acelerar(acelera);
+        acelera = velRandom.Next(1, 21);
+        msgCarroB = CarroB.acelerar(acelera);
+        acelera = velRandom.Next(1, 21);
+        msgCarroC = CarroC.acelerar(acelera);
+        Console.WriteLine($"{i + 1}- {CarroA.owner}:{msgCarroA}  {CarroB.owner}:{msgCarroB}{CarroC.owner}:{msgCarroC} ");
+
+    }
+
+    if (CarroA.GetVelocidadActual() > CarroB.GetVelocidadActual() & CarroA.GetVelocidadActual() > CarroC.GetVelocidadActual())
+    {
+        Console.WriteLine($"{CarroA.owner} Ganooo esta manga!!!");
+        contadorA = contadorA + 1;
+
+    }
+    if (CarroB.GetVelocidadActual() > CarroA.GetVelocidadActual() & CarroB.GetVelocidadActual() > CarroC.GetVelocidadActual())
+    {
+        Console.WriteLine($"{CarroB.owner} Ganooo esta manga!!!");
+        contadorB = contadorB + 1;
+    }
+    if (CarroC.GetVelocidadActual() > CarroB.GetVelocidadActual() & CarroC.GetVelocidadActual() > CarroA.GetVelocidadActual())
+    {
+        Console.WriteLine($"{CarroC.owner} Ganooo esta manga!!!");
+        contadorC = contadorC + 1;
+    }
+}
+if (contadorA > contadorB & contadorA > contadorC)
+{
+    Console.WriteLine($"El equipo A ganoo la carrera!!!");
+}
+if (contadorB > contadorA & contadorB > contadorC)
+{
+    Console.WriteLine($"El equipo B ganoo la carrera!!!");
+}
+if (contadorC > contadorB & contadorC > contadorA)
+{
+    Console.WriteLine($"El equipo C ganoo la carrera!!!");
+}
+if (contadorA == contadorB & contadorA == contadorC)
+{
+    Console.WriteLine($"La carrera es un empate!!!");
+}
